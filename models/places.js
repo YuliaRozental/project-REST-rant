b0a2cce0820e9c3a978c608929439d37f6dcbf25
Yulia <yuyarozental@gmail.com>
2022-08-18 15:18:07 -0400

    new places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/rest.jpeg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/rest2.jpeg'
+}]
