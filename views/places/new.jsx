42d1247dd4f0488f5ab69cd4190254d35ec7f3c7
Yulia <yuyarozental@gmail.com>
2022-08-18 14:54:07 -0400

    Jazzed up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -4,8 +4,39 @@ const Def = require('../default')
 function new_form () {
     return (
         <Def>
-          <main>
+        <main>
             <h1>Add a New Place</h1>
+            <form method="GET" action="/places">
+              <div className="row">
+                <div className="form-group col-sm-6">
+                  <label htmlFor="name">Place Name</label>
+                  <input className="form-control" id="name" name="name" required />
+                </div>
+                <div className="form-group col-sm-6">
+                  <label htmlFor="pic">Place Picture</label>
+                  <input className="form-control" id="pic" name="pic" />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="city">City</label>
+                  <input className="form-control" id="city" name="city" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="state">State</label>
+                  <input className="form-control" id="state" name="state" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="founded">Founded Year</label>
+                  <input type="number" className="form-control" id="founded" name="founded" defaultValue={new Date().getFullYear()} />
+                </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="cuisines">Cuisines</label>
+                <input className="form-control" id="cuisines" name="cuisines" required />
+              </div>
+              <input className="btn btn-primary" type="submit" value="Add Place" />
+            </form>
           </main>
         </Def>
     )
