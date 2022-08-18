88a3655a0a19fa42043eddb0eac91d978cc7c557
Yulia <yuyarozental@gmail.com>
2022-08-18 12:41:46 -0400

    Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -7,13 +7,13 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/rest.jpeg'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/rest2.jpeg'
       }]
       
       res.render('places/index', { places })
--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,7 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <img src="/images/smile.jpg" width="700" height="600" alt="Sorry we can't find your page" />
           </main>
       </Def>
     )
