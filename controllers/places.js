155a0af979700a75f2272b71a6ef41ba9bb94872
Yulia <yuyarozental@gmail.com>
2022-08-20 13:04:36 -0400

    new commit

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,5 @@
-const router = require('express').Router()
-const db = require('../models')
+const router = require('express').Router();
+const db = require('../models');
 
 router.get('/', (req, res) => {
     db.Place.find()
@@ -7,11 +7,10 @@ router.get('/', (req, res) => {
       res.render('places/index', { places })
     })
     .catch(err => {
-      console.log(err) 
+      console.log('err', err)
       res.render('error404')
     })
 })
-
 
 router.post('/', (req, res) => {
   db.Place.create(req.body)
@@ -24,13 +23,19 @@ router.post('/', (req, res) => {
   })
 })
 
-
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
 router.get('/:id', (req, res) => {
-  res.send('GET /places/:id stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
 router.put('/:id', (req, res) => {
--- models/places.js
+++ models/places.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  pic: String,
+  pic: { type: String, default: 'http://placekitten.com/350/350'},
   cuisines: { type: String, required: true },
   city: { type: String, default: 'Anytown' },
   state: { type: String, default: 'USA' },
